9604e98558220a2774c003f8bd656db8d0fcee67
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-23 08:44:08 -0600

    completed forEach, map, filter and reduce methods

--- code.js
+++ code.js
@@ -1,38 +1,40 @@
 var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
-
-/* 
- Use the forEach method to add the name of each planet
- to a div element in your HTML with an id of "planets".
-*/
 var el = document.getElementById("planets");
 
+
+// Use the forEach method to add the name of each planet to a div element in your 
+//  HTML with an id of "planets".
+//  [* forEach needs to invoke a supplied function once per array element or once per
+//  object property but it doesn't change the array. *]
 planets.forEach(function (item) {
   el.innerHTML += item + " ";
 });
 
-el.innerHTML += "<br/>";
+el.innerHTML += "<br/><br/>";
 
-// Use the map method to create a new array where the first letter of each planet is capitalized
+// Use the map method to create a new array where the first letter of each planet is
+//  capitalized. [* Map takes a list of values (an array or object), invokes a function 
+//  on each of those values, and returns the new updated list in the same order. *]
+var capitalizedPlanetsMap = [];
 
-planets.map(function (element) {
-  el.innerHTML += element[0].toUpperCase() + element.slice(1) + " ";
+capitalizedPlanetsMap += planets.map(function (element) {
+  return " " + element[0].toUpperCase() + element.substring(1);
 });
 
-el.innerHTML += "<br/>";
+el.innerHTML += capitalizedPlanetsMap + "<br/><br/>";
 
 
 // Use the filter method to create a new array that contains planets with the letter 'e'
-
 var ePlanets = planets.filter(function (element) {
-  if (element.includes("e")) {
-    return element;
-  }
+    return element.includes("e");
 });
 
 el.innerHTML += ePlanets;
 
+el.innerHTML += "<br/><br/>"
 
 // Use the reduce method to create a sentence from the words in the following array
 var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
-el.innerHTML += "<br/>" + words.reduce(function(previousVal, currentVal) {
+el.innerHTML += words.reduce(function(previousVal, currentVal) {
   return previousVal +" " + currentVal;});
+
